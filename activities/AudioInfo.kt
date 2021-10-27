d5eb4e40b7485d55e26be3dac97dd080acb0718a
rizkymutiaracitraayuti <82300311+rizkymutiaracitraayuti@users.noreply.github.com>
2021-10-27 08:42:38 +0700

    Add files via upload

---
+++ activities/AudioInfo.kt
@@ -0,0 +1,12 @@
+package com.example.projectkuliah.activities
+
+class AudioInfo {
+
+    var Title:String?=null
+    var SongURL:String?=null
+    constructor(Title:String, SongURL:String){
+        this.Title = Title
+        this.SongURL = SongURL
+    }
+
+}
